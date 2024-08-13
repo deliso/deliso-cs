d3e3960ff946a44e4a3c76b6bae33345993f391e
deliso <s.moraleslopez.bcn@gmail.com>
2024-08-13 18:09:57 +0200

    ignore dist

--- dist/assets/js/a7456010.ac21389f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdeliso_cs=self.webpackChunkdeliso_cs||[]).push([[1235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
--- dist/build/assets/js/aba21aa0.9ba15d00.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdeliso_cs=self.webpackChunkdeliso_cs||[]).push([[5742],{7093:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
